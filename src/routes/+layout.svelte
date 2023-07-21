1cf2d3e5e6fa8492169cca79577a97f5db5eebfc
Marcos Freire <marcosdefontes@gmail.com>
2023-07-21 15:55:23 -0300

    feat(home): update home page header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,20 @@
 </script>
 
 <AppShell>
-	<svelte:fragment slot="header">Header</svelte:fragment>
+	<svelte:fragment slot="header">
+		<div class="flex items-center justify-between px-6 py-2">
+			<img
+				src="/android-chrome-512x512.png"
+				alt="Melhor Preço Logo"
+				srcset=""
+				class="h-auto w-10"
+			/>
+			<h1 class="text-lg font-bold uppercase text-primary-500">
+				Melhor <span class="text-tertiary-700">Preço</span>
+			</h1>
+		</div>
+	</svelte:fragment>
 	<!-- Router Slot -->
 	<slot />
-	<svelte:fragment slot="footer">Footer</svelte:fragment>
+	<svelte:fragment slot="footer">Melhor preço</svelte:fragment>
 </AppShell>
