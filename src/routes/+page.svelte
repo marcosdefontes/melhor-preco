6240502d57dafbbb52ad530ca81acebd6feeb406
Marcos Freire <marcosdefontes@gmail.com>
2023-07-21 14:25:48 -0300

    refactor(home): reset home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,2 @@
 <script>
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button type="button" class="btn variant-filled">Button</button>
-<button class="btn variant-filled-primary">primary</button>
